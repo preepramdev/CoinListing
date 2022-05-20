f5d2955f0c24feabd1240b55d28623f721971543
preepramdev <preepramdev@gmail.com>
2022-05-21 02:58:09 +0700

    - refactor top tier

--- app/src/main/java/com/maxnimal/coin/listing/presentation/coin/list/adapter/TopRankItemAdapter.kt
+++ app/src/main/java/com/maxnimal/coin/listing/presentation/coin/list/adapter/TopRankItemAdapter.kt
@@ -1,9 +1,13 @@
 package com.maxnimal.coin.listing.presentation.coin.list.adapter
 
 import android.annotation.SuppressLint
+import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.maxnimal.coin.listing.R
 import com.maxnimal.coin.listing.databinding.ItemTopRankBinding
 import com.maxnimal.coin.listing.domain.model.CoinModel
 import com.maxnimal.coin.listing.presentation.extension.loadImageFromUrl
@@ -48,6 +52,43 @@ class TopRankItemAdapter : RecyclerView.Adapter<TopRankItemAdapter.TopRankItemVi
             ivCoinIcon.loadImageFromUrl(coin.iconUrl)
             tvCoinName.text = coin.name
             tvCoinSymbol.text = coin.symbol
+            tvCoinChange.apply {
+                val dec = DecimalFormat("#,##0.00")
+                val change = dec.format(coin.change)
+                val textColor = when {
+                    coin.change > 0.0 -> {
+                        "#13BC24"
+                    }
+                    coin.change < 0.0 -> {
+                        "#F82D2D"
+                    }
+                    else -> null
+                }
+                textColor?.let { _textColor ->
+                    this.visibility = View.VISIBLE
+                    text = change.replace("-", "")
+                    setTextColor(Color.parseColor(_textColor))
+                } ?: run {
+                    this.visibility = View.GONE
+                }
+            }
+            ivChangeArrow.apply {
+                val arrowIconResId = when {
+                    coin.change > 0.0 -> {
+                        R.drawable.ic_arrow_up_green
+                    }
+                    coin.change < 0.0 -> {
+                        R.drawable.ic_arrow_down_red
+                    }
+                    else -> null
+                }
+                arrowIconResId?.let { _arrowIconResId ->
+                    this.visibility = View.VISIBLE
+                    setImageDrawable(ContextCompat.getDrawable(this.context, _arrowIconResId))
+                } ?: run {
+                    this.visibility = View.GONE
+                }
+            }
         }
     }
 }
