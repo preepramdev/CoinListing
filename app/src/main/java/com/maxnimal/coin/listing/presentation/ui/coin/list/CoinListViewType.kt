e1710c85f8a65f2233ad2f1d5410c7b90f64cbf7
pram <pram@appsynth.net>
2023-05-29 21:27:51 +0700

    improve header adapter

--- app/src/main/java/com/maxnimal/coin/listing/presentation/ui/coin/list/CoinListViewType.kt
+++ app/src/main/java/com/maxnimal/coin/listing/presentation/ui/coin/list/CoinListViewType.kt
@@ -3,6 +3,5 @@ package com.maxnimal.coin.listing.presentation.ui.coin.list
 enum class CoinListViewType(val value: Int) {
     TOP_RANK(111),
     OTHERS(222),
-    HEADER_TOP_RANK(333),
-    HEADER_BUY_SELL_HOLD(444);
+    TEXT_HEADER(333);
 }
